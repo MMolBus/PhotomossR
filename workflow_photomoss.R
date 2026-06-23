033cbf9fd590f9c92337d57628fc07d9c34c2d21
MMolBus <36168270+MMolBus@users.noreply.github.com>
2026-06-23 19:35:06 +0200

    workflow: workflow_photomoss.R Add workflow script for PhotomossR analysis
    Add workflow_photomoss.R: a compact analysis workflow that installs/loads librarian, installs PhotomossR from the MMolBus GitHub dev branch, and uses MButils. The script sets the working directory, generates a chart from vis TIFFs, reads saved chart data, collects ROI files, converts ROIs to polygons, and runs ccspectral.df with SR index and mean descriptor (custom thresholding options provided). Includes commented legacy install snippets for reference.

---
+++ workflow_photomoss.R
@@ -0,0 +1,55 @@
+remove.packages("PhotomossR")
+if(require(librarian)!=T){
+  install.packages('librarian')
+  require(librarian)
+}
+
+
+# librarian::shelf(devtools, MMolBus/photomoss, MMolBus/MButils)
+librarian::shelf(devtools, MMolBus/MButils)
+
+
+devtools::install_github("MMolBus/PhotomossR@dev")
+repo <- getpath()
+devtools::install(repo)  
+
+
+
+# if(require(devtools)!=T){
+#   install.packages('devtools')
+#   require(devtools)
+# }else{
+#   library(devtools)
+# }
+# # Then we install photomoss from my GitHub branch:
+#   
+#   install_github("MMolBus/photomoss")
+# library(photomoss)
+
+  
+  wd <- getpath() #your working directory
+  setwd (wd)
+  tif.path <- getwd()
+  
+  chart2(paste0(tif.path,"/vis"), pic.format = "tif")
+  
+chart <- readRDS(list.files(pattern = "chart"))  
+
+
+roi.paths <- list.files(path = "./rois",pattern=".roi$",full.names = T, recursive = T)
+
+obs.areas <- lapply(roi.paths, roi2polygon, tif.path)
+
+ccspectral.df(tif.path,
+              chart,
+              obs.areas,
+              pdf = F,
+              calculate.thresh = F,
+              descrip = F,
+              manual.mask.test = F,
+              index. = c("SR"),
+              threshold.method = c("Li"),
+              threshold.vector = c(0.6),
+              descriptors. = 
+                c("mean")
+)
